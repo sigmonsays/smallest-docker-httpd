2557b89ec0d45c3355fd0fb03c7509873e061209
Sig Lange <sig.lange@gmail.com>
2023-02-05 22:05:19 -0800

    info script

--- run.sh
+++ run.sh
@@ -3,7 +3,3 @@
 docker rm -f httpd
 docker run -d --name httpd httpd /httpd
 
-container_ip="$(docker inspect httpd | jq -r '.[] | .NetworkSettings.IPAddress')"
-container_pid="$(docker inspect -f '{{.State.Pid}}' httpd)"
-echo "container pid $container_pid"
-echo "container ip $container_ip"
